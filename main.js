59037daa872f8c1703d9cd131b6a941827056b6a
DoidosMB <148970373+DoidosMB@users.noreply.github.com>
2024-11-30 19:59:04 -0300

    b

---
+++ main.js
@@ -0,0 +1,14 @@
+function criarCartao(pergunta,resposta){
+    let conteudo = document.getElementById("conteudo")
+ let cartao = document.createElement('article')
+ cartao.classList = 'cartao'
+ 
+ 
+ cartao.innerHTML = `<div class="cartao--conteudo">
+         <div class="cartao--pergunta"><p>${pergunta}</p></div>
+          <div class="cartao--resposta"><p>${resposta}</p></div>
+         </div>`
+ 
+ conteudo.appendChild(cartao)
+ 
+ }
---
+++ pergunta.js
@@ -0,0 +1,24 @@
+criarCartao( 
+    "Maior artilheiro do corinthians de 2024?",
+    "Yuri Alberto: 12 gols em 24 jogos."
+)
+criarCartao( 
+    "Jogador mais caro do futebol brasileiro de 2024",
+    "Thiago Almada (Botafogo) - R$ 137,4 milhões em 2024."
+)
+criarCartao( 
+    "Clube que mais revelou jogadores?",
+    "São Paulo é quem mais revela jogadores no País"
+)
+criarCartao( 
+    "Quem foi o campeão da copa paulista de 2021?",
+    "O campeonato teve como campeão o São Paulo"
+)
+criarCartao( 
+    "Quem foi o primeiro campeão brasileiro?",
+    "O Cruzeiro foi o primeiro campeão nesse formato."
+)
+criarCartao( 
+    "Clube com mais títulos nacionais?",
+    "Flamengo e Grêmio"
+)
